152c21294bbb5a3c9cd488f8c9f33306bda205a0
Ben <bennaat@gmail.com>
2024-05-24 17:03:10 +0700

    second commit finish calculator

--- script.js
+++ script.js
@@ -29,44 +29,40 @@ renderProduct(product);
 // clear the form after saving
 document.getElementById("input-form").reset(); 
 
+
 }
 
+
 function renderProduct(product) {
-
-
     const itemContainer = document.getElementById("product-list")
-
-
     // create item product list container
     const itemList = document.createElement("div")
     itemList.className = "flex gap-2"
-
-   
-
-    // create checkbox
+     // create checkbox
     const checkbox = document.createElement("input")
     checkbox.type = "checkbox";
-    checkbox.className = "w-6";
+    checkbox.className = "w-4 mr-4";
 
     // create image
     const img = document.createElement("img");
     img.src = product.image;
     img.alt = product.name;
-    img.className = "rounded-lg shadow-2xl max-h-40 bg-red-950";
+    img.className = "transition duration-300 ease-in-out rounded-lg max-h-40 hover:scale-110";
 
     // create product details container
     const productDetails = document.createElement("div");
+    productDetails.className ="grid grid-cols-[50px_minmax(100px,_1fr)] justify-items-center"
     productDetails.id = "product-details"
     
 
     // create h1 element
     const pName = document.createElement("h1");
-    pName.className = "text-xl text-pink-100"
+    pName.className = "text-xl text-indigo-900 justify-self-start"
     pName.textContent = product.name;
 
     // create p element
     const pPrice = document.createElement("p");
-    pPrice.className = "text-blue-600 text-l"
+    pPrice.className = "text-yellow-800 text-l justify-self-end"
     pPrice.id = "price"
     pPrice.textContent = `Price: $${product.price}`;
 
@@ -79,28 +75,41 @@ function renderProduct(product) {
     productDetails.appendChild(pPrice);
 
     itemList.appendChild(productDetails);
+    itemList.id ="itemList"
     itemContainer.appendChild(itemList);
-
+    
 
 }
+
+function addToCart() {
+    const dashboardList = document.getElementById("dashboard-list");
+    // get all items in the product list
+    const selectedItems = document.querySelectorAll("#product-list > div");
+    // loop through selected items with checkbox
+    selectedItems.forEach((item) => {
+        const checkbox = item.querySelector('input[type="checkbox"]');
+        if (checkbox.checked) {
+            const productDetails = item.querySelector("#product-details");
+            const checkboxInput = productDetails.querySelector('input[type="checkbox"]');
+            productDetails.removeChild(checkboxInput);
+            // move item to dashboard list
+            dashboardList.appendChild(item);
+
+        }
+    });
+}
+
+
 
 
 function accuPrice() {
     let totalPrice = 0;
-
-    const selectedItems = document.querySelectorAll("#product-list > div");
-    
+    const accuedItems = document.querySelectorAll("#dashboard-list > div");
     // loop through selected items with checkbox
-
-    selectedItems.forEach((p) => {
-        const checkbox = p.querySelector('input[type="checkbox"]');
-
-        if(checkbox.checked) {
-            floatedPrice = parseFloat(p.querySelector("p:nth-child(4)").textContent.replace("Price: $",""));
+    accuedItems.forEach((p) => {
+            floatedPrice = parseFloat(p.querySelector("p:nth-child(3)").textContent.replace("Price: $",""));
             console.log(floatedPrice);
             totalPrice += floatedPrice;
-        } 
-
     })
 
     document.querySelector("#result").textContent = totalPrice;
